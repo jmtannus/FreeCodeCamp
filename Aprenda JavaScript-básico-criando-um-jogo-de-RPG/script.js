df16dc4e87cf7b768a6f4e3c0cd2f215914878f7
Janaina <jmtannus@gmail.com>
2024-02-22 23:08:31 -0300

    incrementando compra de armas

--- Aprenda JavaScript-básico-criando-um-jogo-de-RPG/script.js
+++ Aprenda JavaScript-básico-criando-um-jogo-de-RPG/script.js
@@ -16,6 +16,24 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [
+  {
+    name: "stick",
+    power: 5,
+  },
+  {
+    name: "dagger",
+    power: 30,
+  },
+  {
+    name: "claw hammer",
+    power: 50,
+  },
+  {
+    name: "sword",
+    power: 100,
+  },
+];
 
 //criando objetos ao meu array de locais e suas propriedades
 const locations = [
@@ -80,10 +98,21 @@ function buyHealth() {
     health += 10;
     goldText.innerText = gold;
     healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
   }
 }
 
-function buyWeapon() {}
+function buyWeapon() {
+  if (gold >= 30) {
+    gold -= 30;
+    goldText.innerText = gold;
+    text.innerText = "You now have a new weapon.";
+    currentWeapon++;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
+  }
+}
 
 function fightSlime() {}
 
