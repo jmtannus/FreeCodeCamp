ea29b3920b7140014173296dffbfe46fab817de4
Janaina <jmtannus@gmail.com>
2024-02-27 00:13:43 -0300

    implementando buyWeapon

--- Aprenda JavaScript-básico-criando-um-jogo-de-RPG/script.js
+++ Aprenda JavaScript-básico-criando-um-jogo-de-RPG/script.js
@@ -103,13 +103,22 @@ function buyHealth() {
   }
 }
 
+
 function buyWeapon() {
-  if (gold >= 30) {
-    gold -= 30;
-    currentWeapon++;
-    goldText.innerText = gold;
-    let newWeapon = weapons[currentWeapon].name;
-    text.innerText = "You now have a new weapon.";
+  if (currentWeapon < weapons.length - 1) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
   }
 }
 
