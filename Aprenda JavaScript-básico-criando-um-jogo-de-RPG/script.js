38662f674dc902e5fc64d63767efc45a1455e3c0
Janaina <jmtannus@gmail.com>
2024-02-29 18:11:56 -0300

    criando constante de monstros

--- Aprenda JavaScript-básico-criando-um-jogo-de-RPG/script.js
+++ Aprenda JavaScript-básico-criando-um-jogo-de-RPG/script.js
@@ -17,22 +17,15 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 const weapons = [
-  {
-    name: "stick",
-    power: 5,
-  },
-  {
-    name: "dagger",
-    power: 30,
-  },
-  {
-    name: "claw hammer",
-    power: 50,
-  },
-  {
-    name: "sword",
-    power: 100,
-  },
+  { name: "stick", power: 5 },
+  { name: "dagger", power: 30 },
+  { name: "claw hammer", power: 50 },
+  { name: "sword", power: 100 },
+];
+const monsters = [
+  { name: "slime", level: 2, health: 15 },
+  { name: "fanged beast", level: 8, health: 60 },
+  { name: "dragon", level: 20, health: 300},
 ];
 
 //criando objetos ao meu array de locais e suas propriedades
@@ -119,6 +112,18 @@ function buyWeapon() {
     }
   } else {
     text.innerText = "You already have the most powerful weapon!";
+  }
+}
+
+function sellWeapon() {
+  if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "Don't sell your only weapon!";
   }
 }
 
